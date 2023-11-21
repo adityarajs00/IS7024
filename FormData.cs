37a967aa4efe1caff84b0c891afb66071fdce642
ShivaniPilli <pillisn@mail.uc.edu>
2023-11-20 20:37:33 -0500

    Feedback form table

---
+++ FormData.cs
@@ -0,0 +1,15 @@
+﻿namespace Neighborhood_Watch
+{
+    public class FormData
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public string City { get; set; }
+        public string SelectedNeighborhood { get; set; }
+        public int SafetyRating { get; set; }
+        public string Comments { get; set; }
+    }
+}
--- Pages/Feedback.cshtml.cs
+++ Pages/Feedback.cshtml.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Neighborhood_Watch.Pages
 {
+
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
@@ -11,9 +15,67 @@ namespace Neighborhood_Watch.Pages
         {
             _logger = logger;
         }
-
+        public List<FormData> NeighborhoodData { get; private set; }
         public void OnGet()
         {
+            NeighborhoodData = GetNeighborhoodData();
+        }
+
+        private List<FormData> GetNeighborhoodData()
+        {
+            // Specify the path to your JSON file
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "neighborhood_data.json");
+
+            // Read JSON data from the file
+            return System.IO.File.Exists(jsonFilePath)
+                ? JsonConvert.DeserializeObject<List<FormData>>(System.IO.File.ReadAllText(jsonFilePath))
+                : new List<FormData>();
+        }
+
+
+
+
+        public IActionResult OnPost()
+        {
+            // Retrieve form data from the request
+            var formData = new FormData
+            {
+                FullName = Request.Form["fullName"],
+                Email = Request.Form["email"],
+                PhoneNumber = Request.Form["phoneNumber"],
+                Address = Request.Form["address"],
+                SelectedNeighborhood = Request.Form["neighborhood"],
+                SafetyRating = Convert.ToInt32(Request.Form["safety_rating"]),
+                Comments = Request.Form["comments"]
+            };
+
+            // Save form data to a JSON file
+            SaveToJsonFile(formData);
+
+            // You can perform additional processing or redirect the user after saving the data
+            return RedirectToPage("/Index"); // Redirect to the home page for example
+        }
+
+        private void SaveToJsonFile(FormData formData)
+        {
+            // Specify the path to your JSON file
+            string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "neighborhood_data.json");
+
+            // Read existing JSON data, or create an empty list if the file doesn't exist
+            List<FormData> existingData = System.IO.File.Exists(jsonFilePath)
+             ? JsonConvert.DeserializeObject<List<FormData>>(System.IO.File.ReadAllText(jsonFilePath))
+             : new List<FormData>();
+
+
+            // Add the new form data to the list
+            existingData.Add(formData);
+
+            // Write the updated list back to the JSON file
+            System.IO.File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(existingData, Formatting.Indented));
         }
     }
 }
+
+
+
+
