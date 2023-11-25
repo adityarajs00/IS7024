7ee327a577e81dd6cb86cfb2b5ce51b9e7baa970
ishaan chaudhari <chaudhia@mail.uc.edu>
2023-11-25 13:30:03 -0500

    Api Controller added for data hosting

---
+++ ApiData/StateRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Neighborhood_Watch.ApiData
+{
+    public class StateRepository
+    {
+        static StateRepository()
+        {
+            allStates = new List<StateDataModel>();
+        }
+
+        public static IList<StateDataModel> allStates { get; set; }
+    }
+    
+}
